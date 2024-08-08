7b8a2abe81a1b48b8ff2189e231a2a9fb0b53b12
abidunalim <abidunalim27@gmail.com>
2024-08-08 20:19:17 +0700

    first commit

---
+++ daftar_buku.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var txt;
+    if(confirm("Maaf buku belum tersedia")) {
+        txt = "";
+    } else{
+        txt = "";
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
